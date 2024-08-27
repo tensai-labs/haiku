e2992c75f77404f099e025616dfdc3bd6c3d5a08
Bob <wastchenko.constantin@gmail.com>
2024-08-26 23:36:00 -0400

    Implement sqlite-vec lib

--- src/utils/db_manager.rs
+++ src/utils/db_manager.rs
@@ -2,13 +2,25 @@ use tokio_rusqlite::Connection;
 
 use crate::types::config_types::Config;
 
+use rusqlite::ffi::sqlite3_auto_extension;
+use sqlite_vec::sqlite3_vec_init;
+
 pub struct DbManager;
 
 impl DbManager {
     pub async fn init_db(config: &Config) -> eyre::Result<Connection> {
+        unsafe {
+            sqlite3_auto_extension(Some(std::mem::transmute(sqlite3_vec_init as *const ())));
+        }
+
         let database =
             DbManager::open_connection(&config.haiku.metadata.database_url.clone()).await?;
-        DbManager::create_tables(&database, config).await?;
+
+        // Sanity check
+        DbManager::sqlite_vec_version(&database).await?;
+
+        DbManager::create_primary_tables(&database).await?;
+        DbManager::create_key_tables(&database, config).await?;
 
         tracing::info!("Database created successfully");
 
@@ -23,7 +35,26 @@ impl DbManager {
         Ok(database)
     }
 
-    pub async fn create_tables(database: &Connection, config: &Config) -> eyre::Result<()> {
+    // To do: Add dynamic vector size to embeddings table
+    pub async fn create_primary_tables(database: &Connection) -> eyre::Result<()> {
+        database
+            .call(|db| {
+                db.execute_batch(
+                    "CREATE VIRTUAL TABLE IF NOT EXISTS embeddings USING vec0(embedding float[4]);
+                 CREATE TABLE IF NOT EXISTS text (
+                     id INTEGER PRIMARY KEY,
+                     description TEXT NOT NULL
+                 );",
+                )
+                .map_err(|e| e.into())
+            })
+            .await
+            .expect("Failed to create primary tables");
+
+        Ok(())
+    }
+
+    pub async fn create_key_tables(database: &Connection, config: &Config) -> eyre::Result<()> {
         // Collect all unique keys from storage_keys and retrieval_keys
         let mut all_keys: Vec<String> = Vec::new();
         for event in &config.events {
@@ -65,6 +96,21 @@ impl DbManager {
                 Ok(())
             })
             .await?;
+        Ok(())
+    }
+
+    // Sanity check: sqlite vec extension is loaded
+    pub async fn sqlite_vec_version(database: &Connection) -> eyre::Result<()> {
+        let version: String = database
+            .call(|db| {
+                db.query_row("SELECT vec_version();", [], |row| row.get(0))
+                    .map_err(|e| e.into())
+            })
+            .await
+            .expect("Failed to get vec version");
+
+        println!("Vec version: {}", version);
+
         Ok(())
     }
 }
