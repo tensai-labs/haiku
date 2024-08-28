015b63c8c8fdc9253016534d0fdbae129e1e4f53
tedison <timothyedison@hotmail.com>
2024-08-28 15:11:51 -0400

    Add haiku_event to repo

--- src/actors/event_handler.rs
+++ src/actors/event_handler.rs
@@ -103,6 +103,15 @@ impl EventHandler {
         if prompt_message.timestamp == 0 {
             return Err(eyre!("event received doesn't have a timestamp"));
         }
+
+        if prompt_message.event_tag.is_empty() {
+            return Err(eyre!("event tag is empty"));
+        }
+
+        if prompt_message.event_id == 0 {
+            return Err(eyre!("event tag is empty"));
+        }
+
         self.prompt_sender.send(prompt_message).await?;
 
         Ok(())
